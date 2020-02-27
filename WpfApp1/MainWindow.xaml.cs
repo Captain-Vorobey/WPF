9a2837c1d1fc7e6bd0fc3ef2b512318aa45bcdc0
vorob <vorobyov_egor@bk.ru>
2020-02-27 22:03:14 +0300

    d

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace WpfApp1
 {
     public partial class MainWindow : Window
     {
-        List<Computer> computers;
         public MainWindow()
         {
             InitializeComponent();
-            computers = new List<Computer>(new[] { new Computer("HP", 123, "HP", new DateTime(2000-12-12-00-00-00)),
-            new Computer("HP pavilion", 435, "HP", new DateTime(2000-12-12-00-00-00))});
-            //computersList.ItemsSource = computers;
         }
 
         private void Login_Click(object sender, RoutedEventArgs e)
@@ -69,7 +66,7 @@ namespace WpfApp1
             MessageBox.Show("Test");
         }
 
-        private void Login_Click3(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             ToolTip toolTip = new ToolTip();
             StackPanel toolTipPanel = new StackPanel();
@@ -77,7 +74,66 @@ namespace WpfApp1
             toolTipPanel.Children.Add(new TextBlock { Text = "Её изменения" });
             toolTip.Content = toolTipPanel;
             button3.ToolTip = toolTip;
-            MessageBox.Show("Graphic");
+
+            PathGeometry pathGeom = new PathGeometry();
+            //Path p = new Path();
+
+            LineSegment vertLS = new LineSegment();
+            PathFigure vertPF = new PathFigure();
+            vertPF.StartPoint = new Point(10, 150);
+            vertLS.Point = new Point(10, 10);
+            vertPF.Segments.Add(vertLS);
+            pathGeom.Figures.Add(vertPF);
+
+            LineSegment horLS = new LineSegment();
+            PathFigure horPF = new PathFigure();
+            horPF.StartPoint = new Point(10, 150);
+            horLS.Point = new Point(150, 150);
+            horPF.Segments.Add(horLS);
+            pathGeom.Figures.Add(horPF);
+
+            for (byte i = 2; i < 14; i++)
+            {
+                PathFigure pf = new PathFigure();
+                pf.StartPoint = new Point(i * 10, 155);
+                LineSegment a = new LineSegment();
+                a.Point = new Point(i * 10, 145);
+                pf.Segments.Add(a);
+                pathGeom.Figures.Add(pf);
+            }
+
+            for (byte i = 3; i < 15; i++)
+            {
+                PathFigure pf = new PathFigure();
+                pf.StartPoint = new Point(5, i * 10);
+                LineSegment a = new LineSegment();
+                a.Point = new Point(15, i * 10);
+                pf.Segments.Add(a);
+                pathGeom.Figures.Add(pf);
+            }
+
+            PolyLineSegment vertArr = new PolyLineSegment();
+            vertArr.Points = new PointCollection();
+            vertArr.Points.Add(new Point(10, 10));
+            vertArr.Points.Add(new Point(15, 15));
+            PathFigure vertArrF = new PathFigure();
+            vertArrF.StartPoint = new Point(5, 15);
+            vertArrF.Segments.Add(vertArr);
+            pathGeom.Figures.Add(vertArrF);
+
+            PolyLineSegment horArr = new PolyLineSegment();
+            horArr.Points = new PointCollection();
+            horArr.Points.Add(new Point(150, 150));
+            horArr.Points.Add(new Point(145, 155));
+            PathFigure horArrF = new PathFigure();
+            horArrF.StartPoint = new Point(145, 145);
+            horArrF.Segments.Add(horArr);
+            pathGeom.Figures.Add(horArrF);
+            MessageBox.Show(vertArr.ToString());
+            //p.Data = pathGeom;
+            //p.Stroke = Brushes.Black;
+
+            //grid.Children.Add(p);
         }
 
         private void Login_Click4(object sender, RoutedEventArgs e)
